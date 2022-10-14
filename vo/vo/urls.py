40d49b5156b80b4f9380ce9c771027d62882abba
lr131 <lr131@localhost.localdomain>
2022-10-14 17:42:40 +0800

    add auth

--- vo/vo/urls.py
+++ vo/vo/urls.py
@@ -30,7 +30,5 @@ urlpatterns = [
     path('api/clients/', include(('clients.urls', 'clients'), namespace='clients')),
     path('api/events/', include(('events.urls', 'events'), namespace='events'), name='events'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('login/', user_views.LoginView.as_view()),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('api-auth/', include(('rest_framework.urls', 'rest_framework'), namespace='rest_framework'))
 ]
