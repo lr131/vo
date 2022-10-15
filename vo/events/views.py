d6d4c7a0dadae57381998a7583ff1d33aa8c3cf2
lr131 <lr131@localhost.localdomain>
2022-10-15 15:15:30 +0800

    pagination for events

--- vo/events/views.py
+++ vo/events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, serializers, generics
+from rest_framework import viewsets, serializers, generics, pagination
 
 from .serializers import EventSerializer
 from .models import Event
@@ -7,3 +7,4 @@ from .models import Event
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all().order_by('name')
     serializer_class = EventSerializer
+    pagination_class = pagination.LimitOffsetPagination
